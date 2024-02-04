dbbf8a4cbc39380323eb513b03892d0eb6a476c2
Zeel Patel <pzeel1330@gmail.com>
2024-02-03 19:11:03 -0500

    error

--- footer.php
+++ footer.php
@@ -1,5 +1,5 @@
 <footer>
-        <p>&copy; 2024 TopScorer - Your Learning Mate. All Rights Reserved. for comapnay
+        <p>&copy; 2024 TopScorer - Your Learning Mate. All Rights Reserved.
         </p>
     </footer>
 
