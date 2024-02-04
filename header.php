17c1d7665ab77489be61ca0d86f19be3a6715432
Zeel Patel <pzeel1330@gmail.com>
2024-02-03 19:18:22 -0500

    new files

---
+++ header.php
@@ -0,0 +1,27 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>TopScorer - Your Learning Mate</title>
+    <link rel="icon" type="image/x-icon" href="images/favicon.jpg">
+    <link rel="stylesheet" href="css/style.css">
+</head>
+
+<body>
+
+    <header>
+        <div class="logo">
+
+            <a href="home.php"><img src="images/logo.png" alt="Top Scorer - Your Learning Mate"></a>
+        </div>
+        <nav>
+            <ul>
+                <li><a href="#">Home</a></li>
+                <li><a href="#">Courses</a></li>
+                <li><a href="#">Assignments</a></li>
+                <li><a href="#">Content</a></li>
+            </ul>
+        </nav>
+    </header>
---
+++ login.php
@@ -0,0 +1,34 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>TopScorer - Your Learning Mate</title>
+    <link rel="icon" type="image/x-icon" href="images/favicon.jpg">
+    <link rel="stylesheet" href="css/style.css">
+</head>
+
+<body>
+
+    <header>
+        <div class="logo">
+            <a href="home.php"><img src="images/logo.png" alt="Top Scorer - Your Learning Mate"></a>
+        </div>
+        <nav>
+            <ul>
+            <li><a href="apply.php">Apply Courses</a></li>
+                <li><a href="#">LogIn</a></li>
+            </ul>
+        </nav>
+    </header>
+
+
+
+    <footer>
+        <p>&copy; 2024 TopScorer - Your Learning Mate. All Rights Reserved.</p>
+    </footer>
+
+</body>
+
+</html>
