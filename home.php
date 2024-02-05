0f53d650502997f09b2059650581a7a0f47c55af
Rana Vinay <110671517+VinayRana03@users.noreply.github.com>
2024-02-04 19:42:44 -0500

    Few Changes

--- home.php
+++ home.php
@@ -10,7 +10,7 @@ include('header.php');
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>TopScorer - Transforming Education</title>
+    <title>TopScorer - Transforming Education Vinay Rana</title>
 </head>
 <body>
 
