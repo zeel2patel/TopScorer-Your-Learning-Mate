555a46092dec0eacb986e6933ea19d7e5864d4a2
Rana Vinay <110671517+VinayRana03@users.noreply.github.com>
2024-02-09 16:12:07 -0500

    few changes

---
+++ admin/includes/sidebar.php
@@ -0,0 +1,37 @@
+<nav class="sidebar sidebar-offcanvas" id="sidebar">
+          <ul class="nav">
+            <li class="nav-item nav-profile">
+              <a href="#" class="nav-link">
+                <div class="profile-image">
+                  <img class="img-xs rounded-circle" src="images/faces/face8.jpg" alt="profile image">
+                </div>
+                <div class="text-wrapper">
+                  <p class="profile-name">Vinay</p>
+                  <p class="designation">Vinay@gmail.com</p>
+                </div>
+               
+              </a>
+            </li>
+            <li class="nav-item nav-category">
+              <span class="nav-link">Dashboard</span>
+            </li>
+            <li class="nav-item">
+              <a class="nav-link" href="dashboard.php">
+                <span class="menu-title">Dashboard</span>
+                <i class="icon-screen-desktop menu-icon"></i>
+              </a>
+            </li>
+            <li class="nav-item">
+              <a class="nav-link" data-toggle="collapse" href="#ui-basic1" aria-expanded="false" aria-controls="ui-basic1">
+                <span class="menu-title">Students</span>
+                <i class="icon-people menu-icon"></i>
+              </a>
+              <div class="collapse" id="ui-basic1">
+                <ul class="nav flex-column sub-menu">
+                  <li class="nav-item"> <a class="nav-link" href="add-students.php">Add Students</a></li>
+                  <li class="nav-item"> <a class="nav-link" href="manage-students.php">Manage Students</a></li>
+                </ul>
+              </div>
+            </li>
+          </ul>
+        </nav>
