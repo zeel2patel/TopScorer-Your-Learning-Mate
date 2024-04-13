9c5d53736dbe5247bf9e34a85ab17a51886df9d7
Vinay Rana <pzeel1330@gmail.com>
2024-04-13 16:57:13 -0400

    fianl student side update

--- student/student_session.php
+++ student/student_session.php
@@ -1,4 +1,3 @@
-<<<<<<< HEAD
 <?php
    error_reporting(0);
    @session_start();
@@ -6,13 +5,4 @@
    if (!isset($_SESSION['student_loggedin']) && $_SESSION['student_loggedin'] !== false) {
       header("location:login.php");
    }
-=======
-<?php
-   error_reporting(0);
-   @session_start();
-   include_once '../admin/function/dbConnection.php';
-   if (!isset($_SESSION['student_loggedin']) && $_SESSION['student_loggedin'] !== false) {
-      header("location:login.php");
-   }
->>>>>>> dca3ad7e5005466afc739c01ee917dab6bfcdb2b
 ?>
--- student/theme/footer.php
+++ student/theme/footer.php
@@ -1,10 +1,4 @@
-<<<<<<< HEAD
 <footer class="main-footer" style="background-image:url('theme/dist/img/header_bg.jpg');position: fixed;width: 100%;bottom: 0;font-weight: bold;color: #3c4c66;">
     <strong>Copyright &copy; 2024 <a href="#">TopScorer</a>.</strong>
     All rights reserved.
-=======
-<footer class="main-footer" style="background-image:url('theme/dist/img/header_bg.jpg');position: fixed;width: 100%;bottom: 0;font-weight: bold;color: #3c4c66;">
-    <strong>Copyright &copy; 2024 <a href="#">TopScorer</a>.</strong>
-    All rights reserved.
->>>>>>> dca3ad7e5005466afc739c01ee917dab6bfcdb2b
 </footer>
