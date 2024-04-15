781e99dd6f41a8b4b8391c7f5db47b9d260f6658
Rana Vinay <110671517+VinayRana03@users.noreply.github.com>
2024-04-14 20:51:26 -0400

    Update topscorer_db.sql

--- topscorer_db.sql
+++ topscorer_db.sql
@@ -3,9 +3,9 @@
 -- https://www.phpmyadmin.net/
 --
 -- Host: 127.0.0.1
--- Generation Time: Mar 31, 2024 at 05:34 PM
--- Server version: 10.4.28-MariaDB
--- PHP Version: 8.2.4
+-- Generation Time: Apr 15, 2024 at 02:13 AM
+-- Server version: 10.4.32-MariaDB
+-- PHP Version: 8.2.12
 
 SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
 START TRANSACTION;
@@ -38,13 +38,6 @@ CREATE TABLE `announcements` (
   `updatedate` datetime NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
 
---
--- Dumping data for table `announcements`
---
-
-INSERT INTO `announcements` (`id`, `course_id`, `faculty_id`, `title`, `description`, `status`, `createdate`, `updatedate`) VALUES
-(6, 1, 32, 'Testing Announcements', 'Hi this is testing Announcement so ignore this data', '1', '2024-03-31 09:49:00', '2024-03-31 09:49:00');
-
 -- --------------------------------------------------------
 
 --
@@ -67,7 +60,17 @@ CREATE TABLE `assignments` (
 --
 
 INSERT INTO `assignments` (`id`, `sub_id`, `assignment_name`, `due_date`, `rubrics`, `assignment_folder`, `status`, `create_date`) VALUES
-(1, 1, 'Assessment Demo 1', '2024-04-30', 'uploads/assignments/Assessment 1testing.pdf', 'uploads/assignments/Assessment 1testing.pdf', '1', '2024-03-31');
+(1, 1, 'Assessment Demo 1', '2024-04-30', 'uploads/assignments/Assessment 1testing.pdf', 'uploads/assignments/Assessment 1testing.pdf', '1', '2024-03-31'),
+(2, 1, 'Assingment Demo 2', '2024-04-30', 'uploads/assignments/Assingment Demo 2testing.pdf', 'uploads/assignments/Assingment Demo 2TopScorer.pdf', '1', '2024-04-04'),
+(3, 1, 'Assingment Demo 3', '2024-04-29', 'uploads/assignments/Assingment Demo 3testing.pdf', 'uploads/assignments/Assingment Demo 3TopScorer.pdf', '1', '2024-04-04'),
+(4, 1, 'Assingment Demo 4', '2024-04-28', 'uploads/assignments/Assingment Demo 4testing.pdf', 'uploads/assignments/Assingment Demo 4TopScorer.pdf', '1', '2024-04-04'),
+(5, 2, 'Assingment Demo 11\r\n', '2024-05-31', 'uploads/assignments/Assingment Demo 1testing.pdf', 'uploads/assignments/Assingment Demo 1TopScorer.pdf', '1', '2024-04-04'),
+(6, 3, '2d Graphic Assignment', '2024-05-31', 'uploads/assignments/2d Graphic AssignmentAssessment 1testing.pdf', 'uploads/assignments/2d Graphic AssignmentAssingment Demo 1testing.pdf', '1', '2024-04-09'),
+(7, 4, 'assignment1', '2024-04-30', 'uploads/assignments/assignment1Summary.pdf', 'uploads/assignments/assignment1Summary.pdf', '1', '2024-04-14'),
+(8, 4, 'assignment2', '2024-04-30', 'uploads/assignments/assignment2form_signed.pdf', 'uploads/assignments/assignment2form_signed.pdf', '1', '2024-04-14'),
+(9, 4, 'assignment3', '2024-04-24', 'uploads/assignments/assignment3form_signed.pdf', 'uploads/assignments/assignment3form_signed.pdf', '1', '2024-04-14'),
+(10, 4, 'assignment4', '2024-05-15', 'uploads/assignments/assignment42 side_signed.pdf', 'uploads/assignments/assignment4form_signed.pdf', '1', '2024-04-14'),
+(11, 4, 'assignment5', '2024-04-30', 'uploads/assignments/assignment4form_signed.pdf', 'uploads/assignments/assignment4form_signed.pdf', '1', '2024-04-15');
 
 -- --------------------------------------------------------
 
@@ -88,7 +91,17 @@ CREATE TABLE `assignments_student` (
 --
 
 INSERT INTO `assignments_student` (`id`, `stud_id`, `assignments_id`, `comment`, `submission_folder`) VALUES
-(1, 30, 1, 'This is testing upload', 'uploads/assignmentsSubmit/30_TopScorer.pdf');
+(1, 30, 1, 'This is testing upload', 'uploads/assignmentsSubmit/30_TopScorer.pdf'),
+(2, 30, 2, 's', 'uploads/assignmentsSubmit/30_2_TopScorer.pdf'),
+(3, 30, 3, 'This is tesing assessment upload', 'uploads/assignmentsSubmit/30_3_TopScorer.pdf'),
+(4, 30, 4, 'This is tesing assessment upload', 'uploads/assignmentsSubmit/30_4_TopScorer.pdf'),
+(5, 30, 5, 'This is tesing assessment upload', 'uploads/assignmentsSubmit/30_5_TopScorer.pdf'),
+(7, 37, 6, 'ggg', 'uploads/assignmentsSubmit/37_6_34_1_Super Magnet.pdf'),
+(8, 38, 7, 'done', 'uploads/assignmentsSubmit/38_7_Summary.pdf'),
+(9, 38, 8, 'done', 'uploads/assignmentsSubmit/38_8_form_signed.pdf'),
+(10, 38, 9, 'done', 'uploads/assignmentsSubmit/38_9_form_signed.pdf'),
+(11, 38, 10, 'done', 'uploads/assignmentsSubmit/38_10_form_signed.pdf'),
+(12, 38, 11, 'done', 'uploads/assignmentsSubmit/38_11_form_signed.pdf');
 
 -- --------------------------------------------------------
 
@@ -99,9 +112,19 @@ INSERT INTO `assignments_student` (`id`, `stud_id`, `assignments_id`, `comment`,
 CREATE TABLE `attendance` (
   `attendance_id` int(11) NOT NULL,
   `enrollment_id` int(11) NOT NULL,
+  `course_id` int(11) NOT NULL,
   `attendance_date` date NOT NULL,
   `status` enum('present','absent') NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+
+--
+-- Dumping data for table `attendance`
+--
+
+INSERT INTO `attendance` (`attendance_id`, `enrollment_id`, `course_id`, `attendance_date`, `status`) VALUES
+(1, 1, 1, '2024-04-11', 'present'),
+(2, 1, 1, '2024-04-12', 'absent'),
+(3, 4, 1, '2024-04-15', 'present');
 
 -- --------------------------------------------------------
 
@@ -125,7 +148,7 @@ CREATE TABLE `courses` (
 
 INSERT INTO `courses` (`course_id`, `course_name`, `course_description`, `start_date`, `end_date`, `status`, `semester_id`) VALUES
 (1, '3D Computer Animation', 'Computer animation has evolved to become a major component in the field of animation production and contributes towards shaping content for film, television, games and online applications. As such, students must be actively engaged in all aspects of the production process and understand the requirements associated with the production pipeline. Traditional design, drawing and storytelling skills are enhanced via the latest computer (software/hardware), combined with 3D animation skills and the creation of characters and customized environments contribute towards the creation of unique content that graduates will utilize in their e-portfolio of project work. \r\n', '2024-03-26', '2024-06-30', '1', 1),
-(2, ' 2D Computer Animation', '2D Co', '2024-03-26', '2025-03-26', '0', 1),
+(2, ' 2D Computer Animation', '2D Co', '2024-03-19', '2025-03-31', '1', 1),
 (5, 'Administrative Business Management', 'Senior management require strong administrative and analytical support, relying on assistants, business, and office managers with technical competency, superior organizational skills, the ability to manage conflicting demands and a variety of responsibilities. Graduates will be able to coordinate activities on the executive\'s behalf and ensure the executive successfully navigates through a multitude of functions and responsibilities. Students will complete courses designed to develop proficiency in the use of integrated software and financial processes, apply managerial principles and processes, strengthen verbal and written communication, and develop expertise in administrative procedures and processes. Graduates of this program will be ready to become integral parts of the leadership team in any business by providing support for the activity of the senior executive group. Interested in taking this program on a part-time basis? It\'s possible to complete this program while still working full-time and fulfilling your other commitments. For more information, please visit our Continuing Education Administrative Business Management program.', '2024-03-30', '2025-03-31', '0', 3);
 
 -- --------------------------------------------------------
@@ -146,7 +169,10 @@ CREATE TABLE `enrollments` (
 --
 
 INSERT INTO `enrollments` (`id`, `stud_id`, `course_id`, `enrollment_date`) VALUES
-(1, 30, 1, '2024-03-31');
+(1, 30, 1, '2024-03-31'),
+(3, 37, 2, '2024-04-09'),
+(4, 38, 1, '2024-04-14'),
+(5, 39, 2, '2024-04-14');
 
 -- --------------------------------------------------------
 
@@ -155,12 +181,29 @@ INSERT INTO `enrollments` (`id`, `stud_id`, `course_id`, `enrollment_date`) VALU
 --
 
 CREATE TABLE `grades` (
-  `grade_id` int(11) NOT NULL,
+  `id` int(11) NOT NULL,
   `enrollment_id` int(11) NOT NULL,
   `assignment_id` int(11) NOT NULL,
   `grade` decimal(5,2) NOT NULL,
   `grade_date` date NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+
+--
+-- Dumping data for table `grades`
+--
+
+INSERT INTO `grades` (`id`, `enrollment_id`, `assignment_id`, `grade`, `grade_date`) VALUES
+(1, 1, 1, 20.00, '2024-04-05'),
+(2, 1, 2, 19.00, '2024-04-05'),
+(3, 1, 3, 18.00, '2024-04-05'),
+(4, 1, 4, 10.00, '2024-04-05'),
+(5, 1, 5, 20.00, '2024-04-05'),
+(6, 3, 6, 20.00, '2024-04-09'),
+(7, 4, 7, 20.00, '2024-04-14'),
+(8, 4, 8, 20.00, '2024-04-15'),
+(9, 4, 9, 20.00, '2024-04-15'),
+(10, 4, 10, 20.00, '2024-04-15'),
+(11, 4, 11, 15.00, '2024-04-15');
 
 -- --------------------------------------------------------
 
@@ -202,8 +245,8 @@ CREATE TABLE `subject` (
 --
 
 INSERT INTO `subject` (`sub_id`, `sub_name`, `sub_description`, `sub_img`, `course_id`, `faculty_id`) VALUES
-(1, 'Anatomy and Life Drawing', 'Description:\r\nThis course will enhance the student’s awareness of the relationships between human and character development, with specific focus on refinement of life-drawing skills. Emphasis on structural drawing techniques and sequential figure drawing will contribute towards an understanding of human/skeletal anatomy and how movement and animation of characters evolve. Students will design and create a unique character that will be utilized in concurrent and future animation projects.\r\n\r\nHours: 42\r\nCredits: 3\r\nPre-Requisites:\r\nCoRequisites:', 'uploads/subject/Anatomy and Life Drawing1.png', 1, 32),
-(2, 'Story Development I', 'Description: Students will research, create and develop stories that will form the basis of their personal e-portfolio production toolkit. These stories will be integrated in concurrent and future courses and become the scripting component of storyboarding for shot selection and composition.\r\nHours: 56\r\nCredits: 4\r\nPre-Requisites:\r\nCoRequisites:', 'uploads/subject/Story Development I1.png', 1, 32);
+(4, 'Animation', 'Animation', 'uploads/subject/AnimationScreenshot 2024-04-10 180603.png', 1, 40),
+(6, 'story development', 'story', 'uploads/subject/story developmenthm6.png', 1, 41);
 
 -- --------------------------------------------------------
 
@@ -229,8 +272,10 @@ CREATE TABLE `users` (
 
 INSERT INTO `users` (`id`, `name`, `mobile`, `email`, `password`, `user_type`, `status`, `createdate`, `updatedate`) VALUES
 (1, 'admin', '', 'admin@gmail.com', '$2y$10$InIGG7Hhvaxf4IDVfY.YC.8mSJA3m.xvHc.XiZALUJEyUNXAZ4Vgi', 'admin', '1', '2020-06-12', '2024-03-20'),
-(30, 'Student1', '9999009090', 'std1@gmail.com', '$2y$10$OcsWNI0fNEHQrAPbTJN/hefJ2Mz5o8eVA/IAN0RXu0Bj2aQY2Jqxe', 'student', '1', '2024-03-31', '2024-03-31'),
-(32, 'faculty1', '9090909090', 'f1@gmail.com', '$2y$10$fjTV9kHlmOOSpIQZV2IyhuLTyVU9DFaONaA9W8sugWP4dizLntaby', 'faculty', '1', '2024-03-31', '2024-03-31');
+(40, 'vinay', '741852963', 'vinay@gmail.com', '$2y$10$f3yyAfBj5B8FPmlNf6glXu/hEscS.m01INDFC8ry9P2JrbDsSNgHS', 'faculty', '1', '2024-04-14', '2024-04-14'),
+(41, 'Abhi', '1425369870', 'abhi@gmail.com', '$2y$10$AjGTGHxm4g8Bj572axFrlO2Fj/OwKNHzeCkroaHBsbXuPu494nfkO', 'faculty', '1', '2024-04-14', '2024-04-14'),
+(39, 'pooja', '1425369870', 'pooja@gmail.com', '$2y$10$mratkPrUQh6mbfKty/O.3u/wIfVCYe.XpmDDjxkzoMn1zfyjL4R0e', 'student', '0', '2024-04-14', '2024-04-14'),
+(38, 'Zeel', '1425369870', 'zeel@gmail.com', '$2y$10$WOfALtu2WBahSUlYpBGBYumeWnlPhj.QoHKssEI7jDszH30HW2kY2', 'student', '1', '2024-04-14', '2024-04-14');
 
 --
 -- Indexes for dumped tables
@@ -276,7 +321,7 @@ ALTER TABLE `enrollments`
 -- Indexes for table `grades`
 --
 ALTER TABLE `grades`
-  ADD PRIMARY KEY (`grade_id`);
+  ADD PRIMARY KEY (`id`);
 
 --
 -- Indexes for table `semesters`
@@ -310,19 +355,19 @@ ALTER TABLE `announcements`
 -- AUTO_INCREMENT for table `assignments`
 --
 ALTER TABLE `assignments`
-  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=2;
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=12;
 
 --
 -- AUTO_INCREMENT for table `assignments_student`
 --
 ALTER TABLE `assignments_student`
-  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=2;
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=13;
 
 --
 -- AUTO_INCREMENT for table `attendance`
 --
 ALTER TABLE `attendance`
-  MODIFY `attendance_id` int(11) NOT NULL AUTO_INCREMENT;
+  MODIFY `attendance_id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=4;
 
 --
 -- AUTO_INCREMENT for table `courses`
@@ -334,13 +379,13 @@ ALTER TABLE `courses`
 -- AUTO_INCREMENT for table `enrollments`
 --
 ALTER TABLE `enrollments`
-  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=2;
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=6;
 
 --
 -- AUTO_INCREMENT for table `grades`
 --
 ALTER TABLE `grades`
-  MODIFY `grade_id` int(11) NOT NULL AUTO_INCREMENT;
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=12;
 
 --
 -- AUTO_INCREMENT for table `semesters`
@@ -352,13 +397,13 @@ ALTER TABLE `semesters`
 -- AUTO_INCREMENT for table `subject`
 --
 ALTER TABLE `subject`
-  MODIFY `sub_id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=3;
+  MODIFY `sub_id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=7;
 
 --
 -- AUTO_INCREMENT for table `users`
 --
 ALTER TABLE `users`
-  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=33;
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=42;
 COMMIT;
 
 /*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
